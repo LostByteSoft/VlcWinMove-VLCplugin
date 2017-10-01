5986e2d023bd5425f5b7ab6f38af920214e7ec6b
LostByteSoft <mad_mike_41@hotmail.com>
2017-10-01 09:16:30 -0400

    Delete Ins_Plugin.bat

--- Ins_Plugin.bat
+++
@@ -1,8 +0,0 @@
-@echo ----------------------------------------------------------
-cd "%~dp0"
-copy "*.exe" "C:\Program Files\"
-copy "*.ico" "C:\Program Files\"
-copy "*.lnk" "C:\Documents and Settings\All Users\Start Menu\Programs\"
-@echo ----------------------------------------------------------
-@pause
-@exit
